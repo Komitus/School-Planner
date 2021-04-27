dc0bbc70bd7ecbc236afbca662dc638bf95b60d0
wojtek <strzelecki54@gmail.com>
2021-04-27 20:33:11 +0200

    Dodałem panel nawigacyjny oraz ładowanie fragmentów.

--- app/src/main/java/com/example/mobileapp/MainActivity.kt
+++ app/src/main/java/com/example/mobileapp/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.mobileapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.mobileapp.fragments.CourseFragment
+import com.example.mobileapp.fragments.GradesFragment
+import com.example.mobileapp.fragments.PlanFragment
+import com.example.mobileapp.fragments.PlusFragment
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val gradesFragment = GradesFragment()
+        val plusFragment = PlusFragment()
+        val planFragment = PlanFragment()
+        val courseFragment = CourseFragment()
+
+        makeCurrentFragment(gradesFragment)
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.ic_oceny -> makeCurrentFragment(gradesFragment)
+                R.id.ic_plan -> makeCurrentFragment(planFragment)
+                R.id.ic_plus -> makeCurrentFragment(plusFragment)
+                R.id.ic_przedmioty -> makeCurrentFragment(courseFragment)
+            }
+            true
+        }
+    }
+
+    private fun makeCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper, fragment)
+            commit()
     }
 }
---
+++ app/src/main/java/com/example/mobileapp/fragments/PlusFragment.kt
@@ -0,0 +1,23 @@
+package com.example.mobileapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.mobileapp.R
+
+
+class PlusFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_plus, container, false)
+    }
+
+
+}
